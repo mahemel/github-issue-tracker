e0418e86e37da6004c83ff6e95c07c8c907c2cf9
mahemel <mahbubhasanhemel@gmail.com>
2026-03-07 03:54:23 +0600

    initial data fetching functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,55 @@
-const getInputValue = (element) => {
-    const input = document.getElementById(element);
-    const inputValue = input.value;
 
-    return inputValue;
+const removeActiveState = () => {
+    const issueBtns = document.querySelectorAll('.issue-btns .btn');
+    
+    issueBtns.forEach(btn => {
+        btn.classList.remove('active')
+    })
 }
 
-document.getElementById('signInBtn').addEventListener('click', () => {
-    const usernameValue = getInputValue('username');
-    const passwordValue = getInputValue('password');
+const issueBtns = document.getElementById('issue-btns');
+if(issueBtns) {
+    issueBtns.addEventListener('click', (event) => {
+        const targetBtn = event.target.closest('.btn');
 
-    if(usernameValue === 'admin' && passwordValue === 'admin123') {
-        window.location.assign('dashboard.html')
-    }
-    else {
-        alert('Incorrect username or password.');
-        return;
-    }
-})
+        if(targetBtn) {
+        const targetType = targetBtn.getAttribute('id');
+            removeActiveState();
+
+            targetBtn.classList.add('active');
+            loadAllIssues(targetType);
+        }
+    });
+}
+
+const loadAllIssues = async () => {
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
+
+    const issues = await fetch(url);
+    const json = await issues.json();
+
+    const data = json.data;
+
+    displayIssues(data);
+}
+
+const displayIssues = (issues) => {
+    const issueContainer = document.getElementById('issueContainer');
+    issueContainer.innerHTML = '';
+
+    const issueCounter = document.getElementById('issue-counter');
+    issueCounter.innerText = issues.length;
+
+    issues.forEach(issue => {
+        const card = document.createElement('div');
+        card.classList.add('border', 'shadow-sm');
+
+        card.innerHTML = `
+            <p>${issue.title}</p>
+        `;
+        
+        issueContainer.appendChild(card)
+    });
+}
+
+loadAllIssues();
---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+const getInputValue = (element) => {
+    const input = document.getElementById(element);
+    const inputValue = input.value;
+
+    return inputValue;
+}
+const signInBtn = document.getElementById('signInBtn');
+if(signInBtn) {
+    signInBtn.addEventListener('click', () => {
+        const usernameValue = getInputValue('username');
+        const passwordValue = getInputValue('password');
+
+        if(usernameValue === 'admin' && passwordValue === 'admin123') {
+            window.location.assign('dashboard.html')
+        }
+        else {
+            alert('Incorrect username or password.');
+            return;
+        }
+    })
+}
